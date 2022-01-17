2ab11b5ed5e5420cc5a214e2a0fa37a9664cd94e
Rafa Mel <melrcm@gmx.net>
2022-01-17 11:44:13 +0100

    feat: initialize FS, WiFi, AP, and MDNS

---
+++ src/configuration.h
@@ -0,0 +1,11 @@
+/* Services */
+#define HOST_NAME "camera"
+
+/* Access Point */
+#define ENABLE_ACCESS_POINT true
+#define ACCESS_POINT_SSID "Camera"
+
+/* WiFi Connection */
+#define ENABLE_WIFI_CONNECTION false
+#define WIFI_CONNECTION_SSID ""
+#define WIFI_CONNECTION_PASSWORD ""
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,48 @@
+#include <WiFi.h>
+#include <ESPmDNS.h>
+#include <SPIFFS.h>
 #include "Arduino.h"
+
+#include "configuration.h"
 
 void setup() {
   // Serial port for debugging purposes
   Serial.begin(115200);
   Serial.setDebugOutput(true);
-  Serial.println("Start");
+  Serial.println();
+
+  /* Initialize SPIFFS */
+  if (SPIFFS.begin(true)) {
+    Serial.println("FS: start");
+  } else {
+    Serial.println("FS: an error has occurred while mounting SPIFFS");
+    ESP_ERROR_CHECK(ESP_FAIL);
+  }
+
+  /* WiFi Connection */
+  #if ENABLE_WIFI_CONNECTION
+    WiFi.begin(WIFI_CONNECTION_SSID, WIFI_CONNECTION_PASSWORD);
+    Serial.print("Connecting to WiFi: ");
+    Serial.println(WIFI_CONNECTION_SSID);
+  #endif
+
+  /* Access Point */
+  #if ENABLE_ACCESS_POINT
+    WiFi.softAP(ACCESS_POINT_SSID);
+    IPAddress AP_IP = WiFi.softAPIP();
+    Serial.print("Access Point SSID: ");
+    Serial.println(ACCESS_POINT_SSID);
+    Serial.print("Access Point IP: ");
+    Serial.println(AP_IP);
+  #endif
+
+  /* Initialize MDNS */
+  if (MDNS.begin(HOST_NAME)) {
+    Serial.print("MDNS Host: ");
+    Serial.println(HOST_NAME);
+  } else {
+    Serial.println("Error setting up MDNS");
+  }
 }
 
 void loop() {
